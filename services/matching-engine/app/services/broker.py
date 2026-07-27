8d6c416bcf18e63f304f24d569f9758ce61efba7
jhe48 <jhe48@buffalo.edu>
2026-07-27 16:37:43 -0400

    feat: implement publisher for redis client

--- services/matching-engine/app/services/broker.py
+++ services/matching-engine/app/services/broker.py
@@ -6,9 +6,14 @@ to broadcast and consume domain events across services.
 """
 
 import os
-
 import redis
+from dotenv import load_dotenv
+import json
 
+load_dotenv()
+
+# Module-level variable for Singleton Pattern for REDIS Client
+_client = None
 
 def get_redis_client():
     """Create and return a Redis client instance.
@@ -19,10 +24,19 @@ def get_redis_client():
         - REDIS_PASSWORD  (optional)
 
     Returns:
-        None — TODO: implement actual Redis client creation.
+        Client — actual Redis client.
     """
     # TODO: build and return a redis.Redis client using env vars.
-    return None
+    global _client
+    if not _client:
+        try:
+            new_client = redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), password=os.getenv("REDIS_PASSWORD"))
+            _client = new_client
+            return _client
+        except Exception as e:
+            print(f"Cannot Create Redis Client: {e}")
+    else: 
+        return _client
 
 
 async def publish_event(channel: str, payload: dict) -> None:
@@ -32,12 +46,14 @@ async def publish_event(channel: str, payload: dict) -> None:
         channel: The Redis pub/sub channel name.
         payload: Dictionary payload to serialise and publish.
 
-    Raises:
-        NotImplementedError: Method not yet implemented.
     """
-    raise NotImplementedError
+    client = get_redis_client()
+    payload_json = json.dumps(payload)
+    client.publish(channel, payload_json)
 
 
+'''
+TODO: This client is being used as a publisher not a subscriber.
 async def subscribe_to_channel(channel: str) -> None:
     """Subscribe to a Redis channel and process incoming messages.
 
@@ -48,3 +64,4 @@ async def subscribe_to_channel(channel: str) -> None:
         NotImplementedError: Method not yet implemented.
     """
     raise NotImplementedError
+'''
