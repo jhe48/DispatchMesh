41b4d3bd4d76d4aa855eb8dc8c3efc1bf55517e3
jhe48 <jhe48@buffalo.edu>
2026-08-22 19:52:10 -0400

    fix: correct incorrect env variable in redisUrl variable

--- services/web-gateway/src/services/broker.ts
+++ services/web-gateway/src/services/broker.ts
@@ -1,6 +1,6 @@
 import Redis from 'ioredis';
 
-const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+const redisUrl = `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`;
 
 export const subscriber = new Redis(redisUrl);
 
