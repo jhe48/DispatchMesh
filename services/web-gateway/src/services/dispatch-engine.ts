66a5e4b1c2007dce9ac6b0e1b8a72706dc3507bb
jhe48 <jhe48@buffalo.edu>
2026-08-26 16:41:53 -0400

    fix: remove potential body checking error on cancelTrip

--- services/web-gateway/src/services/dispatch-engine.ts
+++ services/web-gateway/src/services/dispatch-engine.ts
@@ -37,11 +37,7 @@ export class DispatchEngine {
    * Cancel an in-progress or pending trip.
    */
   async cancelTrip(tripId: string): Promise<void> {
-      const cancel_trip_response = await fetch(`http://matching-engine:8000/trip/${tripId}/cancel`, {
-        method: `PUT`,
-        headers: { 'Content-Type': 'application/json' },
-        body: tripId
-      });
+      const cancel_trip_response = await fetch(`http://matching-engine:8000/trip/${tripId}/cancel`);
       if (!cancel_trip_response.ok) {
         throw new Error(`Python backend failed with status: ${cancel_trip_response.status}`); 
       }
