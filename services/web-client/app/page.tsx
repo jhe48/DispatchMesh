b40ed6d0c36150cae46058293a5774ee5587ed55
jhe48 <jhe48@buffalo.edu>
2026-09-03 17:23:29 -0400

    feat: set up horizontal screen view for user interactions

--- services/web-client/app/page.tsx
+++ services/web-client/app/page.tsx
@@ -3,13 +3,15 @@ import UserPanel from "./components/UserPanel";
 export default function DispatchDashboard() {
   return (
     <div className="p-8">
-      <h1>Dispatch Dashboard</h1>
-      <br /> 
-      <h2>Rider</h2>
-      <UserPanel token={process.env.TEST_RIDER_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ0ZXN0cmlkZXIxMjMifQ.K38SAgkNbixawbagxvlyMLlNlLWLSD3U7V3HNGPLnX0"} role="rider"/>
-      <br />
-      <h2>Driver</h2>
-      <UserPanel token={process.env.TEST_DRIVER_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiItMSJ9.CV4hUmQ4LxMFRfnf-NBHk2VNgY-vydmQver2M32fzwE"} role="driver" />
+      <div className="flex h-screen">
+        <h1>Dispatch Dashboard</h1>
+        <div className="w-1/2 p-8">
+          <UserPanel token={process.env.TEST_RIDER_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ0ZXN0cmlkZXIxMjMifQ.K38SAgkNbixawbagxvlyMLlNlLWLSD3U7V3HNGPLnX0"} role="rider"/>
+        </div>
+        <div className="w-1/2 p-8">
+          <UserPanel token={process.env.TEST_DRIVER_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiItMSJ9.CV4hUmQ4LxMFRfnf-NBHk2VNgY-vydmQver2M32fzwE"} role="driver" />
+        </div>
+      </div>
     </div>
   )
 }
