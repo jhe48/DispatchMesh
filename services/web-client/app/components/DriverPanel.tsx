ab4349292c1a04084aa437cecc0118d2a91976ae
jhe48 <jhe48@buffalo.edu>
2026-09-08 12:35:04 -0400

    feat: accept UserPanel's websocket

--- services/web-client/app/components/DriverPanel.tsx
+++ services/web-client/app/components/DriverPanel.tsx
@@ -1,1 +1,11 @@
-import UserPanel from "./UserPanel";
+import React from "react"
+
+interface DriverDashboardProps {
+    ws: React.RefObject<WebSocket>;
+}
+
+export default function DriverDashboard({ ws }: DriverDashboardProps) {
+    return (
+        <p> DRIVER </p>
+    );
+}
--- services/web-client/app/components/RiderPanel.tsx
+++ services/web-client/app/components/RiderPanel.tsx
@@ -1,1 +1,11 @@
-import UserPanel from "./UserPanel";
+import React from "react"
+
+interface RiderDashboardProps {
+    ws: React.RefObject<WebSocket>;
+}
+
+export default function RiderDashboard({ ws }: RiderDashboardProps) {
+    return (
+        <p>RIDER</p>
+    );
+} 
