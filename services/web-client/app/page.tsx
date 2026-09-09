5dab5ea46170241fe8b4d7a0d1b472885d0d6017
jhe48 <jhe48@buffalo.edu>
2026-09-09 15:32:58 -0400

    fix: change environment key to load correctly into nextJS

--- services/web-client/app/page.tsx
+++ services/web-client/app/page.tsx
@@ -6,10 +6,10 @@ export default function DispatchDashboard() {
       <div className="flex h-screen">
         <h1>Dispatch Dashboard</h1>
         <div className="w-1/2 p-8">
-          <UserPanel token={process.env.TEST_RIDER_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ0ZXN0cmlkZXIxMjMifQ.K38SAgkNbixawbagxvlyMLlNlLWLSD3U7V3HNGPLnX0"} role="rider"/>
+          <UserPanel token={process.env.NEXT_TEST_RIDER_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ0ZXN0cmlkZXIxMjMifQ.K38SAgkNbixawbagxvlyMLlNlLWLSD3U7V3HNGPLnX0"} role="rider"/>
         </div>
         <div className="w-1/2 p-8">
-          <UserPanel token={process.env.TEST_DRIVER_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiItMSJ9.CV4hUmQ4LxMFRfnf-NBHk2VNgY-vydmQver2M32fzwE"} role="driver" />
+          <UserPanel token={process.env.NEXT_TEST_DRIVER_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiItMSJ9.CV4hUmQ4LxMFRfnf-NBHk2VNgY-vydmQver2M32fzwE"} role="driver" />
         </div>
       </div>
     </div>
