c1584ab11910a187e2f4c309e02231386b7041be
jhe48 <jhe48@buffalo.edu>
2026-09-10 11:58:49 -0400

    fix: add query to check existing trips so riders can't hoard drivers

--- services/matching-engine/app/services/dispatch_engine.py
+++ services/matching-engine/app/services/dispatch_engine.py
@@ -24,65 +24,79 @@ class DispatchEngine:
             new_match_request: contract for a rider requesting a match.
 
         """
-        print(f"Rider {new_match_request.rider_id} is Requesting a Driver!")
-
-        trip_instance = TripState(
-            trip_id = str(uuid.uuid4()),
-            rider_id = new_match_request.rider_id,
-            status = TripStatus.PENDING,
-            pickup_latitude = new_match_request.pickup_latitude,
-            pickup_longitude = new_match_request.pickup_longitude,
-            dropoff_latitude = new_match_request.dropoff_latitude,
-            dropoff_longitude = new_match_request.dropoff_longitude,
-            ride_type = new_match_request.ride_type,
-            created_at = new_match_request.requested_at,
-        )
-        conn = get_connection()
-        cur = conn.cursor()
-        query_driver_status_pending = 'pending'
-        query_driver_status_matched = 'matched'
-        
         try: 
             cur.execute("""
-                SELECT driver_id 
-                FROM Drivers 
-                WHERE status = %s 
-                ORDER BY ST_Distance(geom, ST_SetSRID(ST_MakePoint(%s, %s), 4326)) 
-                LIMIT 1;""", 
-            (query_driver_status_pending, 
-            new_match_request.pickup_longitude, 
-            new_match_request.pickup_latitude))
-            closest_available_driver = cur.fetchone()
-            if not closest_available_driver:
-                print("No Drivers Available...")
+                SELECT trip_id
+                FROM Trips
+                WHERE rider_id = %s AND status = %s
+                LIMIT 1;""",
+                (new_match_request.rider_id, TripStatus.MATCHED))
+            rider_has_trip = cur.fetchone()
+            if rider_has_trip:
+                print("Rider has an Active Trip!")
                 return None
-            trip_instance.driver_id = closest_available_driver[0]
-            trip_instance.status = TripStatus.MATCHED
-            trip_instance.updated_at = datetime.now(timezone.utc)
-            cur.execute("""
-                UPDATE Drivers 
-                SET status = %s 
-                WHERE driver_id = %s;""", 
-                (query_driver_status_matched, 
-                closest_available_driver[0]))
-            cur.execute("""
-                INSERT INTO Trips (trip_id, rider_id, driver_id, status, pickup_location, dropoff_location, ride_type, created_at, updated_at)
-                VALUES (%s, %s, %s, %s, ST_SetSRID(ST_MakePoint(%s, %s), 4326), ST_SetSRID(ST_MakePoint(%s, %s), 4326), %s, %s, %s);""", 
-                (trip_instance.trip_id, 
-                 trip_instance.rider_id, 
-                 trip_instance.driver_id,
-                 trip_instance.status,
-                 trip_instance.pickup_longitude, trip_instance.pickup_latitude,
-                 trip_instance.dropoff_longitude, trip_instance.dropoff_latitude, 
-                 trip_instance.ride_type,
-                 trip_instance.created_at,
-                 trip_instance.updated_at))
-            await publish_event("trip.matched", trip_instance.model_dump(mode='json'))
-            return trip_instance
+            print(f"Rider {new_match_request.rider_id} is Requesting a Driver!")
+
+            trip_instance = TripState(
+                trip_id = str(uuid.uuid4()),
+                rider_id = new_match_request.rider_id,
+                status = TripStatus.PENDING,
+                pickup_latitude = new_match_request.pickup_latitude,
+                pickup_longitude = new_match_request.pickup_longitude,
+                dropoff_latitude = new_match_request.dropoff_latitude,
+                dropoff_longitude = new_match_request.dropoff_longitude,
+                ride_type = new_match_request.ride_type,
+                created_at = new_match_request.requested_at,
+            )
+            conn = get_connection()
+            cur = conn.cursor()
+            query_driver_status_pending = 'pending'
+            query_driver_status_matched = 'matched'
+            
+            try: 
+                cur.execute("""
+                    SELECT driver_id 
+                    FROM Drivers 
+                    WHERE status = %s 
+                    ORDER BY ST_Distance(geom, ST_SetSRID(ST_MakePoint(%s, %s), 4326)) 
+                    LIMIT 1;""", 
+                (query_driver_status_pending, 
+                new_match_request.pickup_longitude, 
+                new_match_request.pickup_latitude))
+                closest_available_driver = cur.fetchone()
+                if not closest_available_driver:
+                    print("No Drivers Available...")
+                    return None
+                trip_instance.driver_id = closest_available_driver[0]
+                trip_instance.status = TripStatus.MATCHED
+                trip_instance.updated_at = datetime.now(timezone.utc)
+                cur.execute("""
+                    UPDATE Drivers 
+                    SET status = %s 
+                    WHERE driver_id = %s;""", 
+                    (query_driver_status_matched, 
+                    closest_available_driver[0]))
+                cur.execute("""
+                    INSERT INTO Trips (trip_id, rider_id, driver_id, status, pickup_location, dropoff_location, ride_type, created_at, updated_at)
+                    VALUES (%s, %s, %s, %s, ST_SetSRID(ST_MakePoint(%s, %s), 4326), ST_SetSRID(ST_MakePoint(%s, %s), 4326), %s, %s, %s);""", 
+                    (trip_instance.trip_id, 
+                    trip_instance.rider_id, 
+                    trip_instance.driver_id,
+                    trip_instance.status,
+                    trip_instance.pickup_longitude, trip_instance.pickup_latitude,
+                    trip_instance.dropoff_longitude, trip_instance.dropoff_latitude, 
+                    trip_instance.ride_type,
+                    trip_instance.created_at,
+                    trip_instance.updated_at))
+                await publish_event("trip.matched", trip_instance.model_dump(mode='json'))
+                return trip_instance
+            except Exception as e:
+                print(f"Database Error during Matching: {e}")
+                return None
         except Exception as e:
-            print(f"Database Error during Matching: {e}")
+            print(f"Database Error during Trip Search: {e}")
             return None
-        finally:
+        finally: 
             if 'cur' in locals():
                 cur.close()
 
